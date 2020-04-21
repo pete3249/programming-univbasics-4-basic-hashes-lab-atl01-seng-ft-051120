5ddfdadbec9a50ac84079114dc184a3db2fdd1ad
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-04-21 15:02:46 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,11 @@ def new_hash
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  my_hash = {
+    :libero => "Jenna",
+    :setter => "Coba",
+    :right_side => "Ali"
+  }
 end
 
 def pioneer
